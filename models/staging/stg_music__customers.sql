9e3212443ae2d59e8c27050569e8ef17b910b01b
Anurag Gupta <anuragguptareso@gmail.com>
2024-04-26 16:08:36 +0000

    modified some staging models

--- models/staging/stg_music__customers.sql
+++ models/staging/stg_music__customers.sql
@@ -1,15 +1,15 @@
-select 
- CustomerId   as customer_id,
- FirstName    as customer_first_name,
- LastName     as customer_last_name,	
- Company      as customer_work_company,	
- Address      as customer_mailing_address,	
- City         as customer_mailing_city,	
- State        as customer_mailing_state,
- Country      as customer_mailing_country,
- PostalCode	  as customer_mailing_zipcode,
- Phone	      as customer_phone_number,	
- Fax	      as customer_fax_number,
- Email	      as customer_email_address,
- SupportRepId as representative_employee_id	
-from {{ source('music', 'customer') }}
+select
+    customerid as customer_id,
+    firstname as customer_first_name,
+    lastname as customer_last_name,
+    company as customer_work_company,
+    address as customer_mailing_address,
+    city as customer_mailing_city,
+    state as customer_mailing_state,
+    country as customer_mailing_country,
+    postalcode as customer_mailing_zipcode,
+    phone as customer_phone_number,
+    fax as customer_fax_number,
+    email as customer_email_address,
+    supportrepid as representative_employee_id
+from {{ source("music", "customer") }}
